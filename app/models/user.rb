1b000bc1f1f37c9c781556681b480020210d7c4e
koki-pro <koki.banana03@gmail.com>
2020-10-07 17:42:24 +0900

    Useモデルのバリデーションの設定　2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname,           presence: true
-  validates :first_name,         presence: true
-  validates :last_name,          presence:true
-  validates :first_name_reading, presence: true
-  validates :last_name_reading,  presence: true 
-  validates :birthday,           presence: true      
+  with_options presence: true do
+    validates :nickname
+    validates :first_name,         format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角文字で入力してください"}
+    validates :last_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角文字で入力してください"}
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カナで入力してください"}
+    validates :last_name_reading,  format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カナで入力してください"}
+    validates :birth_day
+  end       
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, presence: true,
+            format: { with: VALID_PASSWORD_REGEX, message: "は半角6文字以上、英数混合で入力してください"}
+  validates :email, uniqueness: { case_sensitive: true }          
 end
