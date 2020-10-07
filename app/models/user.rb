3bdfcb08ca7e0408d9303e94405592790702d99e
koki-pro <koki.banana03@gmail.com>
2020-10-07 19:56:40 +0900

    新規登録時のエラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :first_name,         format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角文字で入力してください"}
-    validates :last_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角文字で入力してください"}
-    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カナで入力してください"}
-    validates :last_name_reading,  format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カナで入力してください"}
+    validates :first_name,         format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :last_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :last_name_reading,  format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :birth_day
   end       
   VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, presence: true,
-            format: { with: VALID_PASSWORD_REGEX, message: "は半角6文字以上、英数混合で入力してください"}
+            format: { with: VALID_PASSWORD_REGEX, message: "is invalid. Input using half-width English numbers and letters, using more than 6 characters"}
   validates :email, uniqueness: { case_sensitive: true }          
 end
