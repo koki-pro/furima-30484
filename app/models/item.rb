155138b06eea0a817a5237376c0107c3bcc63a63
koki-pro <koki.banana03@gmail.com>
2020-10-08 22:15:06 +0900

    コードの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,13 @@
 class Item < ApplicationRecord
-  with_options presece: true do
-    validates: name
-    validates: introduction
-    validates: category_id
-    validates: condition_id
-    validates: postage_payer_id
-    validates: prefecture_code_id
-    validates: preparation_day_id
-    validates: price 
+  with_options presence: true do
+    validates :name
+    validates :introduction
+    validates :category_id
+    validates :condition_id
+    validates :postage_payer_id
+    validates :prefecture_code_id
+    validates :preparation_day_id
+    validates :price 
   end
 
   belongs_to :user
