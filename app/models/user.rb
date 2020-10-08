f7ab3f18a0888e1befab8053ab33b14122a27b65
koki-pro <koki.banana03@gmail.com>
2020-10-08 15:50:50 +0900

    正規表現の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :first_name,         format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
-    validates :last_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
-    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :last_name_reading,  format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :first_name,         format: { with:  /\A[ぁ-んァ-ン一-龥]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :last_name,          format: { with:  /\A[ぁ-んァ-ン一-龥]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :first_name_reading, format: { with:  /\A[ァ-ン]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :last_name_reading,  format: { with:  /\A[ァ-ン]+\z/, message: 'is invalid. Input full-width katakana characters.' }
     validates :birth_day
   end
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
