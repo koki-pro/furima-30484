dd45b7205f2bcc3f088d8fbfc8c60add11847c9a
koki-pro <koki.banana03@gmail.com>
2020-10-09 15:53:25 +0900

    conditionのActiveHash作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,10 +10,12 @@ class Item < ApplicationRecord
     validates :price 
   end
   validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 }
 
   belongs_to :user
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :condition
 end
