7541a7ea24704095c91a5a6f80e811e8383834b5
koki-pro <koki.banana03@gmail.com>
2020-10-09 15:34:59 +0900

    categoryのActiveHash作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,18 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :introduction
-    validates :category_id
-    validates :condition_id
-    validates :postage_payer_id
-    validates :prefecture_code_id
-    validates :preparation_day_id
+    validates :category
+    validates :condition
+    validates :postage_payer
+    validates :prefecture_code
+    validates :preparation_day
     validates :price 
   end
+  validates :category_id, numericality: { other_than: 1 } 
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
 end
