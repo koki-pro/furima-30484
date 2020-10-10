a6e0536968bac0c8096a56e0b9e2097bb17b845d
koki-pro <koki.banana03@gmail.com>
2020-10-10 14:08:29 +0900

    itemコントローラーのcreateアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-
+    
   end
   
   def new
@@ -8,7 +8,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end  
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :category_id, :condition_id, :postage_payer_id, :prefecture_code_id, :preparation_day_id, :price).merge(user_id: current_user.id)
   end
   
 end
