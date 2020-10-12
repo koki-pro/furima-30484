2eba241a8032445e68d9b9e8cbc612fe742cd6f9
koki-pro <koki.banana03@gmail.com>
2020-10-12 22:21:19 +0900

    ビュー画面の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,6 @@
 class PurchasesController < ApplicationController
   def index
+    
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'purchases', to: 'purchases#index'
   devise_for :users
   root to: "items#index"
   resources :items
