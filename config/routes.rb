681feddfd8103a01fcc67cf4ed66b0998234d20e
koki-pro <koki.banana03@gmail.com>
2020-10-13 11:28:29 +0900

    purchseモデルとデータベースの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'purchases', to: 'purchases#index'
   devise_for :users
   root to: "items#index"
   resources :items
+  resources :purchases, only: :index
 end
