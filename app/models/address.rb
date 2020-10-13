5443f4d27ba89a46bc8dea4ff38c4a4444acbc8f
koki-pro <koki.banana03@gmail.com>
2020-10-13 17:42:35 +0900

    配送先フォームの実装

--- app/models/address.rb
+++ app/models/address.rb
@@ -10,6 +10,8 @@ class Address < ApplicationRecord
 
   validates :prefecture_code_id,  numericality: { other_than: 1 }
 
+  belongs_to :purchase
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture_code
 end
