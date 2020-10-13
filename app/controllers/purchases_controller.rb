ae2afb3ddb3d085ce3531aba8972190575cc258b
koki-pro <koki.banana03@gmail.com>
2020-10-13 21:35:08 +0900

    変数変更

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,16 @@
 class PurchasesController < ApplicationController
-  before_action :set_item, only: [:index]
+  before_action :set_items, only: [:index]
 
   def index
   end
  
+  def create
+    
+  end
+
   private
   
-  def set_item
+  def set_items
     @items = Item.find(params[:item_id])
   end
 end
