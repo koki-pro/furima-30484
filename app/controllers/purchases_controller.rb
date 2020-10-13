aa5c3f0c0b69a6e523c4a9f358d1902a44475a37
koki-pro <koki.banana03@gmail.com>
2020-10-13 19:40:57 +0900

    余白修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,7 +2,6 @@ class PurchasesController < ApplicationController
   before_action :set_item, only: [:index]
 
   def index
-    
   end
  
   private
@@ -10,5 +9,4 @@ class PurchasesController < ApplicationController
   def set_item
     @item = Item.find(params[:item_id])
   end
-
 end
