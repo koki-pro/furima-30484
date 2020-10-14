2d612e9b6f9d6c81b0c1ae2eb07fb5a1bf72d33a
koki-pro <koki.banana03@gmail.com>
2020-10-14 18:20:49 +0900

    クレジットカード機能の準備

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,14 +2,25 @@ class PurchasesController < ApplicationController
   before_action :set_items, only: [:index]
 
   def index
+    
   end
 
   def create
+    address = Address.create(address_params)
+    if address.save
+      render 'items/index' 
+    else
+      render action: :index
+    end
   end
 
   private
 
   def set_items
     @items = Item.find(params[:item_id])
+  end
+
+  def address_params
+    params.permit(:post_code, :prefecture_code_id, :city, :house_number, :building_name, :phone_number).merge(purchase_id: params[:purchase_id])
   end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -3,7 +3,7 @@ class Address < ApplicationRecord
     validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'は、数字3桁＋ハイフン（-）＋数字4桁の形式で入力してください' }
     validates :prefecture_code
     validates :city
-    validates :Address
+    validates :house_number
     validates :building_name
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は、ハイフンは使用せず数字は１０桁または１１桁で入力してください' }
   end
